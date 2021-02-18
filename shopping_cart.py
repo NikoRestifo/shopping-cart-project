23c3489dda41c1f7fc7cd491597dd7deabbb8a3c
NikoRestifo <ndr45@georgetown.edu>
2021-02-18 11:21:16 -0500

    Assemble running total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,16 +25,21 @@ products = [
 # INFO CAPTURE / INPUT
 #
 
+total_price = 0
+
 while True:
     selected_id = input("Please select a product indicator: ") #string
-    #> "DONE"
     if selected_id == "DONE":
         break
     else:
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_products = matching_products[0]
-        print("SELECTED PRODUCT: " + matching_products["name"] + " " + str(matching_products["price"]))
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 
 #
 # INFO DISPLAY / OUTPUT
+#
+
+print("TOTAL PRICE: " + str(total_price))
